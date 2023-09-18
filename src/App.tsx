a910e452a04d27841fd558adb60b72016b623aab
Vladislav Shumkin <vldslv.shumkin@gmail.com>
2023-09-18 13:51:50 +0600

    added mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AppShell, MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core'
+import { useColorScheme } from '@mantine/hooks'
+
+import { TodosPage } from '@/pages/todos'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const preferredColorScheme = useColorScheme((localStorage.getItem('mantine-color-scheme') as ColorScheme) || 'light')
+  const [ colorScheme, setColorScheme ] = useState<ColorScheme>(preferredColorScheme)
+
+  const toggleColorScheme = () => {
+    setColorScheme(prev => prev === 'dark' ? 'light' : 'dark')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider withGlobalStyles theme={{ colorScheme }}>
+        <AppShell>
+          <TodosPage />
+        </AppShell>
+      </MantineProvider>
+    </ColorSchemeProvider>
   )
 }
 
